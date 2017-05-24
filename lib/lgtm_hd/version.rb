e44817e0e960c07c983ebae897abcf7a3e4778ed
Huy Dinh <phradion@gmail.com>
2017-05-24 11:59:24 +0700

    refactored class MemeGenerator with more internal API and compatible with module's configurations

--- lib/lgtm_hd/version.rb
+++ lib/lgtm_hd/version.rb
@@ -1,8 +1,3 @@
 module LgtmHD
   VERSION         = "0.1.0"
-  PROGRAMNAME     = "lgtm_hd"
-  DESCRIPTION     = "Generating images from user input with LGTM text on it, or fetching images from LGTM.in based on user's query. Finally put the image to clipboard."
-
-  TEMP_FILE_PREFIX    = "lgtm_tmp_";
-  FONT_PATH           = "/fonts/impact.ttf";
 end
