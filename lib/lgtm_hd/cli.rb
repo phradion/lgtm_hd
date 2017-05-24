6262b0e0ee46665bf1265fdc163232d71ff84f3d
Huy Dinh <phradion@gmail.com>
2017-05-24 20:33:10 +0700

    Added interactive mode and fixed gem version

--- lib/lgtm_hd/cli.rb
+++ lib/lgtm_hd/cli.rb
@@ -22,38 +22,74 @@ module LgtmHD
 
       command :transform do |c|
         c.syntax = 'lgtm_hd transform <source_uri> <output_uri> [options]'
-        c.summary = 'Generate a LGTM image from source_uri (local path or URL) into output_uri (local)'
+        c.summary = 'Generate a LGTM image from source_uri (local path or URL) into output folder'
         c.description = ''
         c.example '', 'lgtm_hd export http://domain.com/image.png /path/to/lgtm.png'
         c.option '--clipboard', 'Copy the end result (LGTM image) to OS\'s clipboard'
+        c.option '--interactive', 'In case you forgot all these super complexive args and options'
 
         c.action do |args, options|
-          # TODO add validation for args
-          uri = URI.parse(args[0])
-          tmp_file_name = Time.now.strftime('%Y-%m-%d_%H-%M-%S') << LgtmHD::Configuration::TEMP_FILE_PREFIX
+          to_clipboard = options.clipboard
 
-          meme_generator = MemeGenerator.new(*args)
+          # ARGS validation!
+          if args.length == 2 then
+              source_uri = args[0]
+              output_uri = args[1]
+              p output_uri
+
+          elsif options.interactive  # Interactive mode!
+            say "-- LGTM Interactive mode --"
+            source_uri = ask('Source (URL or Path/to/file): ')
+            output_uri = ask('Output Folder: ')
+            to_clipboard = agree("Copy to clipboard afterward? [Y/N]")
+            p output_uri
+          else
+            raise "Too few or too many arguments provided. Need 2: source and output URIs."
+          end
+
+
+          # Validate the inputs
+          unless (source_uri =~ URI::regexp or File.exist?(source_uri)) then
+            # Part of the reason why I fucking hate #unless. No reason.
+            raise "Source is not proper URIs (URL or Path/to/file)"
+          end
+
+          unless File.exist?(output_uri) and File.directory?(output_uri) then
+            raise "Output is not a directory or valid path"
+          end
+
+          file_ext = File.extname(source_uri)
+          output_uri = File.join(output_uri, "" << LgtmHD::Configuration::OUTPUT_PREFIX << Time.now.strftime('%Y-%m-%d_%H-%M-%S') << file_ext)
+
+          # Do stuff with our LGTM meme
+          say "- Reading and inspecting source"
+          meme_generator = MemeGenerator.new(source_uri, output_uri)
+          say "- Rendering output"
           meme_generator.draw
 
+          # Export and play around with the clipboard
+          say "- exporting to file"
           meme_generator.export do |output|
             say "LGTM image has been generated at #{output}."
-            if not options.clipboard then return; end
-            if OS.mac? then
-              applescript "set the clipboard to (read (POSIX file \"#{output}\") as GIF picture)"
-              say "I see you are using MacOSX. Content of the file has been copied to your clipboard."
+            if to_clipboard then
+              if OS.mac? then
+                applescript "set the clipboard to (read (POSIX file \"#{output}\") as GIF picture)"
+                say "I see you are using MacOSX. Content of the file has been copied to your clipboard."
 
-              #{}`osascript -e 'set the clipboard to (read (POSIX file "#{output}") as JPEG picture)'`
-              # Note on 2017/05/22
-              # Currently Github allow pasting image directly to comment box.
-              # However it does not support pure text content produced by pbcopy so we have to use direct Applescript
-              # No Universal solution as for now.
-              #
-              # Apple Script reference: http://www.macosxautomation.com/applescript/imageevents/08.html
-            else
-              Clipboard.copy(output)
-              say "Path to LGTM file has been copied to your clipboard."
-            end
-          end
+                # Apple Script Command reference
+                # Sample: `osascript -e 'set the clipboard to (read (POSIX file "#{output}") as JPEG picture)'`
+                #
+                # Currently Github allow pasting image directly to comment box.
+                # However it does not support pure text content produced by pbcopy so we have to use direct Applescript
+                # No Universal solution as for now.
+                #
+                # Apple Script reference: http://www.macosxautomation.com/applescript/imageevents/08.html
+              else
+                Clipboard.copy(output)
+                say "Path to LGTM file has been copied to your clipboard."
+              end
+            end # end of if to_clipboard
+          end # end of meme_generator.export block
 
         end
 
--- lib/lgtm_hd/configuration.rb
+++ lib/lgtm_hd/configuration.rb
@@ -4,9 +4,8 @@ module LgtmHD
     PROGRAM_NAME      = "lgtm_hd"
     DESCRIPTION       = "Generating images from user input with LGTM text on it, or fetching images from LGTM.in based on user's query. Finally put the image to clipboard."
 
-    TEMP_FILE_PREFIX    = "lgtm_tmp_"
-
     # Output Image configurations
+    OUTPUT_PREFIX    = "lgtm_tmp_"
     OUTPUT_MAX_WIDTH    = 500
     OUTPUT_MAX_HEIGHT   = 500
     OUTPUT_DENSITY      = 90 # or 120 point per inch
