02c0d2ba3638911f552deca1f79390c8f638e346
Huy Dinh <phradion@gmail.com>
2017-05-26 10:09:06 +0700

    #9 alignement fix

--- lib/lgtm_hd/configuration.rb
+++ lib/lgtm_hd/configuration.rb
@@ -1,18 +1,18 @@
 module LgtmHD
   module Configuration
     # Program configurations
-    PROGRAM_NAME      = "lgtm_hd"
-    DESCRIPTION       = "Generating images from user input with LGTM text on it, or fetching images from LGTM.in based on user's query. Finally put the image to clipboard."
+    PROGRAM_NAME          = "lgtm_hd"
+    DESCRIPTION           = "Generating images from user input with LGTM text on it, or fetching images from LGTM.in based on user's query. Finally put the image to clipboard."
 
     # Output Image configurations
-    OUTPUT_PREFIX    = "lgtm_tmp_"
-    OUTPUT_MAX_WIDTH    = 500
-    OUTPUT_MAX_HEIGHT   = 500
-    OUTPUT_DENSITY      = 90 # or 120 point per inch
+    OUTPUT_PREFIX         = "lgtm_hd_"
+    OUTPUT_MAX_WIDTH      = 500
+    OUTPUT_MAX_HEIGHT     = 500
+    OUTPUT_DENSITY        = 90 # or 120 point per inch
 
     # Caption configurations
-    FONT_PATH           = "/fonts/impact.ttf"
-    CAPTION_FONT_SIZE_DEFAULT = 96
+    FONT_PATH                   = "/fonts/impact.ttf"
+    CAPTION_FONT_SIZE_DEFAULT   = 96
 
   end
 end
