ce9ca865402940b4268da9bfe2975b4f3b316d26
Huy Dinh <phradion@gmail.com>
2017-05-28 21:05:38 +0700

    #9 removed unncessary .freeze

--- lib/lgtm_hd/configuration.rb
+++ lib/lgtm_hd/configuration.rb
@@ -3,19 +3,20 @@ module LgtmHD
     # Program configurations
     PROGRAM_NAME          = "lgtm_hd".freeze
     AUTHOR                = "Huy Dinh <phradion@gmail.com>".freeze
-    DESCRIPTION           = "Generating images from user input with LGTM text on it, or fetching images from LGTM.in based on user's query. Finally put the image to clipboard.".freeze
-
+    DESCRIPTION           = "Generating images from user input with LGTM text on it,"\
+                            "or fetching images from LGTM.in based on user's query.\n"\
+                            "Finally put the image to clipboard.".freeze
+    MORE_HELP_URL         = "https://github.com/phradion/lgtm_hd".freeze
+    
     # Output Image configurations
-    OUTPUT_PATH_DEFAULT = File.expand_path("~/Desktop").freeze
-
     OUTPUT_PREFIX         = "lgtm_hd_".freeze
-    OUTPUT_MAX_WIDTH      = 500.freeze
-    OUTPUT_MAX_HEIGHT     = 500.freeze
-    OUTPUT_DENSITY        = 90.freeze # or 120 point per inch
+    OUTPUT_MAX_WIDTH      = 500
+    OUTPUT_MAX_HEIGHT     = 500
+    OUTPUT_DENSITY        = 90 # or 120 point per inch
 
     # Caption configurations
     FONT_PATH                   = "/fonts/impact.ttf".freeze
-    CAPTION_FONT_SIZE_DEFAULT   = 96.freeze
+    CAPTION_FONT_SIZE_DEFAULT   = 96
 
   end
 end
